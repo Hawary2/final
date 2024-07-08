b312e863741d08901b06099155e32a31d28e94ea
Hawary2 <88610825+Hawary2@users.noreply.github.com>
2024-07-08 18:07:51 +0300

    Add files via upload

---
+++ products-index.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include 'seo.php';
+
+$page = 'products-index';
+$seo_data = get_seo_data($page);
+?>
+<?php require 'products/cutes.php'; $cute = query ("SELECT * FROM products"); ?>
+<?php include 'products/headhome.php' ?>
+<?php include 'mainNavbar.php';?>
+<div class="row">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/brouva.svg" type="image/x-icon">
+
+    <!-- Google Web Fonts -->
+    <link <link href="mcss/bootstrap.min.css" rel="stylesheet">
+<link href="mcss/style.css" rel="stylesheet">
+<div class="container-fluid p-0">
+<?php foreach( $cute as $sweet ) : ?>
+<main class="container">
+<div class="col-md-4 p-3"><div class="card p-1">
+<a href="products.php?id=<?=$sweet['id'];?>"> 
+<img class="img-fluid card" alt="<?=$sweet['title'];?>" src="<?=$sweet['cover'];?>"/>
+<div class="card-content p-3">
+<h3><?= $sweet["title"]; ?> ✏️</h3>
+<p>📝 <?= $sweet["description"]; ?></p>
+</div>
+</a>
+</div></div>
+
+<?php endforeach; ?>
+<?php include 'mainFooter.php'; ?>
+
+</div>
